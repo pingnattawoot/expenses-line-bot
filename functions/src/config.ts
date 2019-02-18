49c3095ff4298784cd4884a41ae73cb33969de81
pingnattawoot <nattawoot.s@taskworld.com>
2019-02-19 00:33:45 +0700

    Delete config.ts

--- functions/src/config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  lineSecret: {
-    channelAccessToken:
-      'B42NB7v+Fv56DtM1BXi10Rlxgg9t0RxNCLS9NF8w57qWS4h9DhfCwMYv6FuOR5HaYqWhOti717pMJib8rNbol7UcIoSIeWwbi+yCKBr4zwXMTlHmTwMmnefOLO23YJwlubJo1rGYHoRqcJsMZBNA1QdB04t89/1O/w1cDnyilFU=',
-    channelSecret: '3413e2e93715aa7cef23e2b4dbcbc3b8',
-  },
-}
